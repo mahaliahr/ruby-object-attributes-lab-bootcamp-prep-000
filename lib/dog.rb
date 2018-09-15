9b1bdbeec713f529898ebedfcab319125c88320a
Mahalia Henry-Richards <mahalia_hr@outlook.com>
2018-09-15 17:29:54 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -20,4 +20,20 @@ end
 
 class Person 
   
+    def name
+    @name
+  end 
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def job
+    @job
+  end 
+  
+  def job=(job)
+    @job = job
+  end
+  
 end 
